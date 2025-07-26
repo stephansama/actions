1c516129da2e826cf4e607eeeb86cf005504bdcf
stephansama <stephanrandle@hotmail.com>
2025-07-25 21:32:52 -0400

    chore: updated action implementation

--- do-not-track-polyfill/src/index.ts
+++ do-not-track-polyfill/src/index.ts
@@ -1,3 +1,23 @@
 import * as core from "@actions/core";
 
-core.getInput("additional");
+const telemetryEnvs = {
+	ASTRO_TELEMETRY_DISABLED: 1,
+	DO_NOT_TRACK: 1,
+	NEXT_TELEMETRY_DISABLED: 1,
+	TURBO_TELEMETRY_DISABLED: 1,
+	VERCEL_TELEMETRY_DISABLED: 1,
+	WRANGLER_SEND_METRICS: false,
+	ZX_VERBOSE: 1,
+};
+
+if (process.env.DO_NOT_TRACK) {
+	const additionalEnv: Record<string, number | boolean> = JSON.parse(
+		core.getInput("additional") || "{}",
+	);
+
+	for (const [name, val] of [additionalEnv, telemetryEnvs].flatMap(
+		Object.entries,
+	)) {
+		core.exportVariable(name, val);
+	}
+}
