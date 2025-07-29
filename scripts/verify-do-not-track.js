c8f0024664fd7930790c8cc9f4fdb09616785987
stephansama <stephanrandle@hotmail.com>
2025-07-29 07:13:15 -0400

    chore: fixed workflows

--- scripts/verify-do-not-track.js
+++ scripts/verify-do-not-track.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 
-if (process.env.DO_NOT_TRACK === "1" || process.env.DO_NOT_TRACK === "true") {
+if (process.env.DO_NOT_TRACK !== "1" || process.env.DO_NOT_TRACK !== "true") {
 	process.exit(0);
 }
 
-const envVars = [
+for (const env of [
 	"ASTRO_TELEMETRY_DISABLED",
 	"NEXT_TELEMETRY_DISABLED",
 	"TURBO_TELEMETRY_DISABLED",
 	"VERCEL_TELEMETRY_DISABLED",
 	"WRANGLER_SEND_METRICS",
-];
-
-for (const env of envVars) {
+]) {
 	if (!process.env[env]) throw new Error(`env variable ${env} is not set`);
 	console.info("verified ", env);
 }
