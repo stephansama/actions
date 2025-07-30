501f18420d1a485a20771732652a5b1d736dbc3b
stephansama <stephanrandle@hotmail.com>
2025-07-29 23:00:04 -0400

    chore: created unit tests for multi deployments

--- multi-deployments/src/index.ts
+++ multi-deployments/src/index.ts
@@ -1,43 +1,32 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-const environments = Object.entries(
-	JSON.parse(core.getInput("environments") || "{}"),
-);
-
-const auto_inactive = JSON.parse(
-	core.getInput("invalidate_previous") || "false",
-);
-
-const envs = environments.map(([env]) => env);
-const urls = environments.map(([, url]) => url as string);
-
-const token = process.env.GITHUB_TOKEN;
-
-if (!token) {
-	throw new Error(
-		"failed to load $GITHUB_TOKEN. it is needed to create deploys",
-	);
+if (isInvoked()) {
+	run();
 }
 
-if (!envs.filter(Boolean).length !== !urls.filter(Boolean).length) {
-	throw new Error("the length of environments do not match the urls");
-}
+export async function run() {
+	const { ref, token } = loadEnvVariables();
+	const { auto_inactive, environments } = loadInputs();
+	const { envs, urls } = parseEnvironments(environments);
 
-const octokit = github.getOctokit(token);
+	const octokit = github.getOctokit(token);
 
-const ref = process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF || "";
+	const { owner, repo } = github.context.repo;
 
-async function run() {
+	const commonProps = {
+		auto_inactive,
+		owner,
+		repo,
+	};
+
 	const deploymentData = await Promise.all(
 		envs.map(async (environment) =>
 			octokit.rest.repos.createDeployment({
-				auto_inactive,
+				...commonProps,
 				auto_merge: false,
 				environment,
-				owner: github.context.repo.owner,
 				ref,
-				repo: github.context.repo.repo,
 				required_contexts: [],
 			}),
 		),
@@ -46,34 +35,94 @@ async function run() {
 	await Promise.all(
 		deploymentData.map(async (data) =>
 			octokit.rest.repos.createDeploymentStatus({
-				auto_inactive,
+				...commonProps,
 				deployment_id: parseInt(data.data.id, 10),
-				owner: github.context.repo.owner,
-				repo: github.context.repo.repo,
 				state: "in_progress",
 			}),
 		),
 	);
 
-	const deployments = deploymentData.map((deployment, index) => ({
-		...deployment.data,
-		deployment_url: envs[index],
-	}));
+	const log_url = `https://github.com/${owner}/${repo}/commit/${github.context.sha}/checks`;
 
 	await Promise.all(
-		deployments.map(async (dep, i) =>
+		deploymentData.map(async (dep, i) =>
 			octokit.rest.repos.createDeploymentStatus({
-				auto_inactive,
+				...commonProps,
 				deployment_id: parseInt(dep.id, 10),
 				environment_url: urls[i],
-				log_url: `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/commit/${github.context.sha}/checks`,
-				owner: github.context.repo.owner,
+				log_url,
 				ref,
-				repo: github.context.repo.repo,
 				state: "success",
 			}),
 		),
 	);
 }
 
-run();
+export function isInvoked() {
+	return require.main === module;
+}
+
+export function parseEnvironments(environments: [string, string][]) {
+	const envs = environments.map(([env]) => env).filter(Boolean);
+	const urls = environments.map(([, url]) => url).filter(Boolean);
+
+	if (!envs.length !== !urls.length) {
+		throw new Error("the length of environments do not match the urls");
+	}
+
+	return { envs, urls };
+}
+
+export function loadInputs() {
+	const inputEnvironments = core.getInput("environments");
+
+	if (!inputEnvironments) {
+		throw new Error(
+			"please provide a JSON object containing the target environments to deploy. with the keys being the environment name and the values being the deployment url",
+		);
+	}
+
+	const parsedEnvironments = JSON.parse(inputEnvironments);
+
+	const environments: [string, string][] = Object.entries(parsedEnvironments);
+
+	const auto_inactive: boolean = JSON.parse(
+		core.getInput("invalidate_previous") || "false",
+	);
+
+	return {
+		auto_inactive,
+		environments,
+	};
+}
+
+export function loadEnvVariables() {
+	const token = process.env.GITHUB_TOKEN;
+	const ref =
+		core.getInput("ref") ||
+		process.env.GITHUB_HEAD_REF ||
+		process.env.GITHUB_REF;
+
+	if (!ref && !token) {
+		throw new Error(
+			"failed to load $GITHUB_TOKEN or target commit ref. both are needed to create deploys",
+		);
+	}
+
+	if (!ref) {
+		throw new Error(
+			"failed to load ref to create deploys from. it is needed to create deploys",
+		);
+	}
+
+	if (!token) {
+		throw new Error(
+			"failed to load $GITHUB_TOKEN from head. it is needed to create deploys",
+		);
+	}
+
+	return {
+		ref,
+		token,
+	};
+}
