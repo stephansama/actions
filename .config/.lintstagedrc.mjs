367115f5379069a96b065dd04f98d66122aeaa9d
stephansama <stephanrandle@hotmail.com>
2025-08-13 07:48:34 -0400

    chore: created vitepress website

--- .config/.lintstagedrc.mjs
+++ .config/.lintstagedrc.mjs
@@ -5,8 +5,12 @@ const jsLike = "{js,cjs,mjs,jsx,ts,mts,cts,tsx,astro,svelte,vue}";
  * @type {import('lint-staged').Configuration}
  */
 const config = {
-	[`!(*.${jsLike})`]: "prettier --write",
-	[`*.${jsLike}`]: ["eslint --fix", "prettier --write"],
+	[`!(*.${jsLike})`]: "prettier --write --ignore-unknown",
+	[`*.${jsLike}`]: [
+		"eslint --fix",
+		"prettier --write --ignore-unknown",
+		"vitest related --run",
+	],
 };
 
 export default config;
